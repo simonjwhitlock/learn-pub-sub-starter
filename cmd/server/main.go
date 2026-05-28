ef0843b7b1e783dd22f4d457d68dc73c52c969ea
Simon Whitlock <simonjwhitlock@gmail.com>
2026-05-28 06:40:49 +0100

    rabbitmq message sending

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	const connStr = "amqp://guest:guest@localhost:5672/"
+	rbtSess, err := amqp.Dial(connStr)
+	if err != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
+	}
+	defer rbtSess.Close()
+	fmt.Println("Connection to message service successful.")
+
+	channel, err := rbtSess.Channel()
+	if err != nil {
+		log.Fatal("failed to open new channel: %v", err)
+	}
+
+	msg := routing.PlayingState{
+		IsPaused: true,
+	}
+
+	jsonMsg, err := json.Marshal(msg)
+	if err != nil {
+		log.Fatal("failed to Marshal message: %v", err)
+	}
+
+	err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, jsonMsg)
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Peril server shutting down.")
 }

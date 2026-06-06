4240f576b1e89f532450b8d935752f5400338335
Simon Whitlock <simonjwhitlock@gmail.com>
2026-06-06 12:16:09 +0100

    gj

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
@@ -19,48 +20,75 @@ func main() {
 	}
 	defer rbtSess.Close()
 	fmt.Println("Connection to message service successful.")
-
+	newChan, err := rbtSess.Channel()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	userName, err := gamelogic.ClientWelcome()
 	if err != nil {
-		log.Fatal("Failed to get user name: %v", err)
+		log.Fatalf("Failed to get user name: %v", err)
 	}
 	gameState := gamelogic.NewGameState(userName)
 	chName := routing.PauseKey + "." + gameState.GetUsername()
 
 	err = pubsub.SubscribeJSON(rbtSess, routing.ExchangePerilDirect, chName, routing.PauseKey, pubsub.SimpleQueueType{Name: "transient"}, handlerPause(gameState))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to subscribe to pause feed: %v", err)
 	}
-	moveKey := "army_moves." + gameState.GetUsername()
-	pubsub.DeclareAndBind(rbtSess, routing.ExchangePerilTopic, moveKey, routing.GameLogSlug, pubsub.SimpleQueueType{Name: "transient"})
+
+	moveQueue := "army_moves." + gameState.GetUsername()
+	moveKey := "army_moves.*"
+
+	err = pubsub.SubscribeJSON(rbtSess, routing.ExchangePerilTopic, moveQueue, moveKey, pubsub.SimpleQueueType{Name: "transient"}, handlerMove(gameState))
+	if err != nil {
+		log.Fatalf("Failed to subscribe to move feed: %v", err)
+	}
 
 	for {
 		words := gamelogic.GetInput()
 		if len(words) != 0 {
-			if words[0] == "spawn" {
-				gameState.CommandSpawn(words)
-			} else if words[0] == "move" {
-				gameState.CommandMove(words)
-			} else if words[0] == "status" {
+			switch words[0] {
+			case "spawn":
+				err = gameState.CommandSpawn(words)
+				if err != nil {
+					log.Printf("failed to spawn: %v", err)
+				}
+			case "move":
+				mv, err := gameState.CommandMove(words)
+				if err != nil {
+					log.Printf("failed to move: %v", err)
+				}
+
+				err = pubsub.PublishJSON(newChan, routing.ExchangePerilTopic, moveQueue, mv)
+				if err != nil {
+					log.Printf("Failed to publish move: %v", err)
+				}
+
+			case "status":
 				gameState.CommandStatus()
-			} else if words[0] == "help" {
-				gamelogic.PrintClientHelp()
-			} else if words[0] == "spam" {
+			case "spam":
 				fmt.Println("Spamming not allowed yet!")
-			} else if words[0] == "quit" {
+			case "quit":
 				gamelogic.PrintQuit()
 				break
-			} else {
-				fmt.Println("Error: Unknown command.")
+			default:
+				fmt.Println("invalid command")
 			}
 		}
 	}
-
 }
 
 func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
 	return func(ps routing.PlayingState) {
 		defer fmt.Print("> ")
 		gs.HandlePause(ps)
+	}
+}
+
+func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) {
+	return func(mv gamelogic.ArmyMove) {
+		defer fmt.Print("> ")
+		gs.HandleMove(mv)
 	}
 }
